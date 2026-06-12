ffd4e2193a3f079f3e892e1b02fcac7e2657e1a3
jayvienmocallay <j.mocallay.141340.tc@umindanao.edu.ph>
2026-06-12 08:47:16 +0800

    Add report workflow icons

--- src/ui/icons.tsx
+++ src/ui/icons.tsx
@@ -1,4 +1,5 @@
 import type React from "react";
+import { logoIdentity } from "./designTokens";
 
 type IconProps = {
   className?: string;
@@ -13,42 +14,55 @@ function IconShell({ className = "", title, children }: React.PropsWithChildren<
   );
 }
 
+export function WhiskerWatchLogo({ className = "", title = logoIdentity.accessibleName }: IconProps) {
+  return (
+    <span className={`ww-logo ${className}`} aria-label={title} role="img">
+      <span className="ww-logo-mark" aria-hidden="true">{logoIdentity.mark}</span>
+      <span className="ww-logo-wordmark">{logoIdentity.wordmark}</span>
+    </span>
+  );
+}
+
 export function CollarTagIcon(props: IconProps) {
-  return <IconShell {...props}>◇</IconShell>;
+  return <IconShell {...props}>TAG</IconShell>;
 }
 
 export function PawDropIcon(props: IconProps) {
-  return <IconShell {...props}>◖</IconShell>;
+  return <IconShell {...props}>PIN</IconShell>;
 }
 
 export function WhiskerDivider(props: IconProps) {
-  return <IconShell className={`whisker-divider ${props.className ?? ""}`} title={props.title}>⌁</IconShell>;
+  return <IconShell className={`whisker-divider ${props.className ?? ""}`} title={props.title}>---</IconShell>;
 }
 
 export function AlertEarIcon(props: IconProps) {
-  return <IconShell {...props}>▲</IconShell>;
+  return <IconShell {...props}>!</IconShell>;
 }
 
 export function FoodBowlIcon(props: IconProps) {
-  return <IconShell {...props}>◡</IconShell>;
+  return <IconShell {...props}>BOWL</IconShell>;
 }
 
 export function HelpedPawIcon(props: IconProps) {
-  return <IconShell {...props}>●</IconShell>;
+  return <IconShell {...props}>PAW</IconShell>;
 }
 
 export function CatHeadMarkerIcon(props: IconProps) {
-  return <IconShell {...props}>ᗢ</IconShell>;
+  return <IconShell {...props}>CAT</IconShell>;
 }
 
 export function WatchEyeIcon(props: IconProps) {
-  return <IconShell {...props}>◉</IconShell>;
+  return <IconShell {...props}>EYE</IconShell>;
 }
 
 export function MoonTailIcon(props: IconProps) {
-  return <IconShell {...props}>◜</IconShell>;
+  return <IconShell {...props}>MOON</IconShell>;
 }
 
 export function CameraCatEyeIcon(props: IconProps) {
-  return <IconShell {...props}>◍</IconShell>;
+  return <IconShell {...props}>CAM</IconShell>;
+}
+
+export function TailSignalIcon(props: IconProps) {
+  return <IconShell {...props}>TAIL</IconShell>;
 }
